8da82680043272f9af6991d4bfc3cfc3ef773450
yoshiaki <himajin315@gmail.com>
2014-01-16 15:22:04 +0900

    index.php を追加

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+  <head>
+    <title>DNS インターン記録</title>
+    <link ref="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body>
+  <h1>Dozens インターン記録</h1>
+  <ul>
+    <li><a href="./dns_search.php">DNS Search</a></li>
+  </ul>
+  </body>
+</html>
+
