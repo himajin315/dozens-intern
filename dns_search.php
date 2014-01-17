abb6c053a70c024a39eb7d74e6e021b967e86ff3
Yoshiaki OSHIRO <himajin315@gmail.com>
2014-01-17 14:52:18 +0900

    レコードをテーブルで表示する。

--- dns_search.php
+++ dns_search.php
@@ -3,6 +3,20 @@
     <title>DNS Search</title>
     <meta charset="UTF-8">
     <link ref="stylesheet" href="style.css" type="text/css">
+    <style>
+    table {
+      margin:30px;
+      border-collapse: collapse;
+    }
+    td {
+      border: solid 1px;
+      padding: 0.5em;
+    }
+   .r-type{
+     text-align: center;
+     background-color: #ddd;
+    }
+    </style>
   </head>
   <body>
   <h1>DNS Search</h1>
@@ -14,20 +28,46 @@
      if($_GET['host']){
        $host_name = $_GET['host'];
        //DNSレコードを取得する
-       $result = dns_get_record($host_name);
+       $result = dns_get_record($host_name, DNS_ANY, $authns, $addtl);
        //結果を表示
        if($result){
-         echo "<pre>";
-         print_r($result);
-         echo "</pre>";
+         $ns_hosts = array();
+         echo '<table>';
          foreach ($result as $array){
-           foreach ($array as $key => $value){
-             echo 'key[ '.$key.' ], value[ '.$value.' ]です。<br/>';
+           if($array["type"]=="SOA"){
+             echo '<tr><td colspan=2 class="r-type">SOA records - Source Of Authority</td>';
+             echo '<tr><td>'.$array["host"].'. serial number </td><td>'.$array["serial"].'</td></tr>';
+             echo '<tr><td>'.$array["host"].'. MNAME </td><td>'.$array["mname"].'</td></tr>';
+             echo '<tr><td>'.$array["host"].'. RNAME </td><td>'.$array["rname"].'</td></tr>';
+             echo '<tr><td>'.$array["host"].'. REFRESH </td><td>'.$array["refresh"].'</td></tr>';
+             echo '<tr><td>'.$array["host"].'. RETRY </td><td>'.$array["retry"].'</td></tr>';
+             echo '<tr><td>'.$array["host"].'. TTL </td><td>'.$array["ttl"].'</td></tr>';
            }
-           echo '<hr>';
+           if($array["type"]=="TXT"){
+             echo '<tr><td colspan=2 class="r-type">TXT records</td>';
+             echo '<tr><td>'.$array["host"].'. TXT </td><td>'.$array["txt"].'</td></tr>';
+           }
+           if($array["type"]=="A"){
+             echo '<tr><td colspan=2 class="r-type">A records</td>';
+             echo '<tr><td>'.$array["host"].'. A </td><td>'.$array["ip"].'</td></tr>';
+           }
+           if($array["type"]=="MX"){
+             echo '<tr><td colspan=2 class="r-type">MX records - Mailservers</td></tr>';
+             echo '<tr><td>'.$array["pri"].' '.$array["target"] .'</td><td></td></tr>';
+           }
+           if($array["type"]=="NS"){
+             $ns_hosts[] = $array["target"];
+           }
          }
+         if($ns_hosts){
+           echo '<tr><td colspan=2 class="r-type">NS records - Nameservers</td>';
+           foreach ($ns_hosts as $ns_host){
+             echo '<tr><td>'.$ns_host.'</td><td></td></tr>';
+           }
+         }
+         echo "</table>";
        }else{
-         echo '見つかりませんでした。';
+         echo "見つかりませんでした。";
        }
      }
   ?>
